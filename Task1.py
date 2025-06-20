13abd97ca1476e4204321bbd4b618f3848c81c22
Ashwin06-coder <ashwinmandaokar0609@gmail.com>
2025-06-21 02:12:50 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=input('Enter first value: ')
+b=input('Enter second value: ')
+a=float(a)
+b=float(b)
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("Addition: ",addition)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
